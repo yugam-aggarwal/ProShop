173bfc53a55546fd71ad91fa98fb28d504fb52d2
Yugam Aggarwal <yugamaggarwal007@gmail.com>
2025-01-17 01:03:32 +1100

    before react router dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import Container from "react-bootstrap/esm/Container"
+import { Header }  from "./components/Header.jsx"
+import Footer from "./components/Footer.jsx"
+import HomeScreen from "./screens/HomeScreen.jsx"
 
 const App = () => {
   return (
-    <div>
-      <h1>Welcome to proshop</h1>
-    </div>
+    <>
+      <Header/>
+      <main className="py-3">
+        <Container>
+          <HomeScreen/>
+        </Container>
+      </main>
+      <Footer/>
+    </>
   )
 }
-
 export default App
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,29 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { FaShoppingCart, FaUser } from 'react-icons/fa'
+import  logo  from '../assets/logo.png'
+const Header = () => {
+  return (
+    <>
+      <header>
+        <Navbar bg="dark" variant='dark' expand='lg' collapseOnSelect>
+          <Container>
+            <Navbar.Brand href='/'>
+              <img src={logo} alt="ProShop" />
+              ProShop
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id='basic-nabar-nav'>
+              <Nav className="ms-auto ">
+                <Nav.Link href="/cart"><FaShoppingCart />Cart</Nav.Link>
+                <Nav.Link href='/login'><FaUser /> Sign In</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </header>
+    </>
+  )
+}
+export { Header }
